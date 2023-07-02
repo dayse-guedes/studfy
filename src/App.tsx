8f1bf9f30ec402a0efc00ae9248d0a11fee73f7a
daydocx <contato.dayseguedes@gmail.com>
2023-07-02 08:37:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
