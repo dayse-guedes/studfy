a3c1f88da33dd24ca8d3de8bd855408661e88a7a
daydocx <contato.dayseguedes@gmail.com>
2023-07-07 14:11:36 -0300

    :lipstick: feat: Estilizaçao CSS do formulario

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cronometro from '../Cronometro';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
+
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa | undefined>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores =>
+      tarefasAnteriores.map(tarefa =>
+        tarefa.id === tarefaSelecionada.id
+          ? { ...tarefa, selecionado: true }
+          : { ...tarefa, selecionado: false }
+      )
+    );
+  }
+  
+
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores =>
+        tarefasAnteriores.map(tarefa =>
+          tarefa.id === selecionado.id
+            ? { ...tarefa, selecionado: false, completado: true }
+            : tarefa
+        )
+      );
+    }
+  }
+
   return (
-    <div className= {style.AppStyle}>
-      <Formulario />
-      <Lista />
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Cronometro selecionado={selecionado} finalizarTarefa={finalizarTarefa} />
     </div>
   );
 }
